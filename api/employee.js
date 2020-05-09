cbdcff440e9896349ab4b1a07800755f67017686
Martin <56932913+mmr011@users.noreply.github.com>
2020-05-09 18:08:33 -0400

    conpleted employee routers

--- api/employee.js
+++ api/employee.js
@@ -47,7 +47,7 @@ employeeRouter.post('/', (req, res, next) => {
     const isCurrentEmployee = req.body.employee.isCurrentEmployee === 0 ? 0 : 1; 
     
     if(!name || !position || !wage) {
-        res.sendStatus(400);
+        return res.sendStatus(400);
     };
 
     db.run(
@@ -79,6 +79,68 @@ employeeRouter.post('/', (req, res, next) => {
 
 });
 
+employeeRouter.put('/:employeeId', (req, res, next) => {
+    const name = req.body.employee.name; 
+    const position = req.body.employee.position;
+    const wage = req.body.employee.wage
 
+    const isCurrentEmployee = req.body.employee.isCurrentEmployee === 0 ? 0 : 1; 
+    
+    if(!name || !position || !wage) {
+        return res.sendStatus(400);
+    };
+
+    db.run(
+        'UPDATE Employee SET name = $name, position = $position, wage = $wage, is_current_employee = $isCurrentEmployee WHERE Employee.id = $employeeId', 
+        {
+            $name: name, 
+            $position: position, 
+            $wage: wage, 
+            $isCurrentEmployee: isCurrentEmployee, 
+            $employeeId: req.params.employeeId
+        }, 
+        (err) => {
+            if(err) {
+                next(err);
+            } else {
+                db.get(
+                    `SELECT * FROM Employee WHERE id = ${req.params.employeeId}`, 
+                    (err, updatedEmployee) => {
+                        if(err) {
+                            next(err);
+                        } else {
+                            res.status(200).json({ employee: updatedEmployee });
+                        };
+                    }
+                );
+            };
+        }
+    );
+});
+
+employeeRouter.delete('/:employeeId', (req, res, next) => {
+    db.run(
+        'UPDATE Employee SET is_current_employee = 0 WHERE id = $employeeId', 
+        {
+            $employeeId: req.params.employeeId
+        }, 
+        (err) => {
+            if(err) {
+                next(err);
+            } else {
+                db.get(
+                    `SELECT * FROM Employee WHERE id = ${req.params.employeeId}`, 
+                    (err, notCurrentlyWorking) => {
+                        if(err) {
+                            next(err);
+                        } else {
+                            res.status(200).send({ employee: notCurrentlyWorking });
+                        };
+                    }
+                );
+            };
+        }
+    );
+});
 
 module.exports = employeeRouter; 
